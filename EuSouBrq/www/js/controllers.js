732187292613ad86b497d2f77f372e01be775e6b
Gabriel Schimidt Mylla <gabrielschimidtmylla@hotmail.com>
2016-11-22 17:48:16 -0200

    Login

--- EuSouBrq/www/js/controllers.js
+++ EuSouBrq/www/js/controllers.js
@@ -1,15 +1,51 @@
-(function(){
+(function () {
 
     "use strict";
 
     angular.module("brqApp")
-           .controller("homeController", HomeController);
+        .controller("loginController", LoginController)
+        .controller("menuController", MenuController)
+        .controller("homeController", HomeController)
+        .controller("pontoController", PontoController);
 
-HomeController.$inject = [];
+    MenuController.$inject = ["$ionicPopup", "$state", "$ionicLoading"];
 
-function HomeController()
-{
-    var vm = this;
-}
+    function MenuController($ionicPopup, $state, $ionicLoading) {
+        var vm = this;
+        vm.sair = sair;
+
+        function sair() {
+            $ionicPopup.confirm({
+                title: "Sair",
+                template: "Deseja mesmo sair do aplicativo?"
+            }).then(function (res) {
+                if (res) {
+                    $ionicLoading.show();
+                    setTimeout(function () {
+                        $state.go("login");
+                        $ionicLoading.hide();
+                    }, 2000);
+                }
+            });
+        }
+    }
+
+    LoginController.$inject = [];
+
+    function LoginController() {
+        var vm = this;
+    }
+
+    HomeController.$inject = [];
+
+    function HomeController() {
+        var vm = this;
+    }
+
+    PontoController.$inject = [];
+
+    function PontoController() {
+        var vm = this;
+    }
 
 })();
--- EuSouBrq/www/js/route.js
+++ EuSouBrq/www/js/route.js
@@ -9,18 +9,36 @@
 
     function configuration($stateProvider, $urlRouterProvider) {
 
-        $urlRouterProvider.otherwise("/home");
+        $urlRouterProvider.otherwise("/menu/home");
 
-        $stateProvider.state("menu", {
+        $stateProvider.state("login", {
+            url: "/login",
+            templateUrl: "templates/login.html",
+            controller: "loginController",
+            controllerAs: "vm"
+        }).state("menu", {
             url: "/menu",
             templateUrl: "templates/menu.html",
             controller: "menuController",
             controllerAs: "vm"
-        }).state("home", {
+        }).state("menu.home", {
             url: "/home",
-            templateUrl: "templates/home.html",
-            controller: "homeController",
-            controllerAs: "vm"
+            views:{
+                'menuContent':{
+                    templateUrl: "templates/home.html",
+                    controller: "homeController",
+                    controllerAs: "vm"
+                }
+            }
+        }).state("menu.ponto", {
+            url: "/ponto",
+            views:{
+                'menuContent':{
+                    templateUrl: "templates/ponto.html",
+                    controller: "pontoController",
+                    controllerAs: "vm"
+                }
+            }
         });
     }
 })();
