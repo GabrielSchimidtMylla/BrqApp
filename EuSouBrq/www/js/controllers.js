f196d5a43cb2af03ca0be2cb118bbc7526d41fed
Gabriel Schimidt Mylla <gabrielschimidtmylla@hotmail.com>
2016-11-23 11:50:51 -0200

    ajustes login

--- EuSouBrq/www/js/controllers.js
+++ EuSouBrq/www/js/controllers.js
@@ -15,25 +15,53 @@
         vm.sair = sair;
 
         function sair() {
-            $ionicPopup.confirm({
-                title: "Sair",
-                template: "Deseja mesmo sair do aplicativo?"
-            }).then(function (res) {
-                if (res) {
+
+            navigator.notification.confirm("Deseja mesmo sair do aplicativo?", function (result) {
+                if (result === 1) {
                     $ionicLoading.show();
                     setTimeout(function () {
                         $state.go("login");
                         $ionicLoading.hide();
                     }, 2000);
                 }
-            });
+            }, "Logout", ["Sim", "Cancelar"]);
         }
+        // $ionicPopup.confirm({
+        //     title: "Sair",
+        //     template: "Deseja mesmo sair do aplicativo?"
+        // }).then(function (res) {
+        //     if (res) {
+        //         $ionicLoading.show();
+        //         setTimeout(function () {
+        //             $state.go("login");
+        //             $ionicLoading.hide();
+        //         }, 2000);
+        //     }
+        // });
     }
+
 
     LoginController.$inject = [];
 
     function LoginController() {
         var vm = this;
+        
+        vm.login = login;
+        vm.rememberPass = rememberPass;
+        vm.dados = { };
+
+        function login() {
+            if(vm.formLogin.$valid)
+            {
+                navigator.notification.alert("Logado",function(){
+
+                });
+            }
+        }
+
+        function rememberPass() {
+
+        }
     }
 
     HomeController.$inject = [];
