6868e7cecbfa897be0b79ec7227df9c200c22105
Gabriel Schimidt Mylla <gabrielschimidtmylla@hotmail.com>
2016-11-23 15:10:26 -0200

    esqueci minha senha concluido

--- EuSouBrq/www/js/controllers.js
+++ EuSouBrq/www/js/controllers.js
@@ -1,4 +1,4 @@
-(function () {
+(function() {
 
     "use strict";
 
@@ -8,67 +8,68 @@
         .controller("homeController", HomeController)
         .controller("pontoController", PontoController);
 
-    MenuController.$inject = ["$ionicPopup", "$state", "$ionicLoading"];
+    MenuController.$inject = ["$ionicPopup", "$state", "$ionicLoading", "usuarioService"];
 
-    function MenuController($ionicPopup, $state, $ionicLoading) {
+    function MenuController($ionicPopup, $state, $ionicLoading, usuarioService) {
         var vm = this;
         vm.sair = sair;
 
         function sair() {
 
-            navigator.notification.confirm("Deseja mesmo sair do aplicativo?", function (result) {
+            navigator.notification.confirm("Deseja mesmo sair do aplicativo?", function(result) {
                 if (result === 1) {
-                    $ionicLoading.show();
-                    setTimeout(function () {
-                        $state.go("login");
-                        $ionicLoading.hide();
-                    }, 2000);
+                    usuarioService.sair();
+                    $state.go("login");
                 }
             }, "Logout", ["Sim", "Cancelar"]);
         }
-        // $ionicPopup.confirm({
-        //     title: "Sair",
-        //     template: "Deseja mesmo sair do aplicativo?"
-        // }).then(function (res) {
-        //     if (res) {
-        //         $ionicLoading.show();
-        //         setTimeout(function () {
-        //             $state.go("login");
-        //             $ionicLoading.hide();
-        //         }, 2000);
-        //     }
-        // });
     }
 
 
-    LoginController.$inject = ["usuarioService","$state","applicationConst"];
+    LoginController.$inject = ["usuarioService", "$state", "applicationConst", "$ionicPopup", "$scope"];
 
-    function LoginController(usuarioService, $state, applicationConst) {
+    function LoginController(usuarioService, $state, applicationConst, $ionicPopup, $scope) {
         var vm = this;
 
         vm.login = login;
         vm.rememberPass = rememberPass;
+        vm.loading = false;
         vm.dados = {};
+        $scope.emailRemember = "";
 
         function login() {
             if (vm.formLogin.$valid) {
-
+                vm.loading = true;
                 usuarioService.autenticar(vm.dados.usuario, vm.dados.senha)
-                              .then(function(data){
-                                  
-                                  var valores = JSON.stringify(data.data);
-                                  window.localStorage.setItem(applicationConst.storageUser,valores);
-
-                                  $state.go("menu.home");
-                              })
-                              .catch(function(error){
-                                    navigator.notification.alert(error, function () { });
-                              });
+                    .then(function(data) {
+                        if (data)
+                            $state.go("menu.home");
+                    }).catch(function(error) {
+                        navigator.notification.alert(error, function() { });
+                    }).finally(function() {
+                        vm.loading = false;
+                    });
             }
         }
 
         function rememberPass() {
 
+            $ionicPopup.show({
+                template: '<div class="list"><label class="item item-input"><input type="email" ng-model="emailRemember" placeholder="E-mail"></label></div>',
+                title: 'Relembrar Senha',
+                subTitle: "Insira seu e-mail cadastrado",
+                scope: $scope,
+                buttons: [
+                    { text: 'Cancelar' },
+                    {
+                        text: '<b>Confirmar</b>',
+                        type: 'button-positive',
+                        onTap: function(e) {
+                            navigator.notification.alert("Verifique seu e-mail!", function() { });
+                        }
+                    }
+                ]
+            });
         }
     }
 
--- EuSouBrq/www/js/services.js
+++ EuSouBrq/www/js/services.js
@@ -1,21 +1,38 @@
-(function () {
+(function() {
 
     "use strict";
 
     angular.module("brqApp")
         .service("usuarioService", UsuarioService);
 
-    UsuarioService.$inject = ["$http", "$q", "locationConst"];
+    UsuarioService.$inject = ["$http", "$q", "locationConst", "applicationConst"];
 
-    function UsuarioService($http, $q, locationConst) {
+    function UsuarioService($http, $q, locationConst, applicationConst) {
         var self = this;
 
-        self.autenticar = function (user, password) {
-            if (user != "teste" || password != "teste")
-               return $q(function (resolve,reject) { reject("Usuário ou Senha inválido"); });
+        self.autenticar = function(user, password) {
 
-            return $http.post(locationConst.login, { user: user, password: password });
-        }
+            if (user != "teste" || password != "teste")
+                return $q(function(resolve, reject) { reject("Usuário ou Senha inválido"); });
+
+            return $q(function(resolve, reject) {
+
+                $http.post(locationConst.login, { user: user, password: password })
+                    .then(function(data) {
+
+                        var valores = JSON.stringify(data.data);
+                        window.localStorage.setItem(applicationConst.storageUser, valores);
+
+                        resolve(true);
+                    }).catch(function(error) {
+                        reject("Erro ao realizar o login! Verifique sua conexão com a internet.");
+                    });
+            });
+        };
+
+        self.sair = function() {
+            window.localStorage.removeItem(applicationConst.storageUser);
+        };
 
         return self;
     }
